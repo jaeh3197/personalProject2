e805d54534cd7017c6eb49b1a89d28df8aa82a4d
jaeh3197 <jaeh3197@naver.com>
2024-10-23 17:26:29 +0900

    feat : 유효성 검사 추가
    유효성을 검사하는 메소드를 만드는 과정에서 입력받은 숫자를 나머지를 구하여 배열로 저장 하는 방식이 많은 오류가 있음을 알게 되었습니다. 따라서 입력값을 문자형으로 입력 받아 숫자로 변환하여 저장하는 방식으로 수정하였습니다. validateInput 메소드를 추가하여 유효성 검사가 좀 더 깔끔하게 처리되도록 수정하였습니다.

--- BaseballGame.java
+++ BaseballGame.java
@@ -1,8 +1,12 @@
 package numplay;
 
 import java.util.*;
+import java.util.regex.Pattern;
 
 public class BaseballGame {
+
+    //입력값이 유효한지 검사하기 위해 전역변수 선언
+    String input;
     //리스트를 전역변수로 선언
     ArrayList<Integer> correctNum;
     ArrayList<Integer> inputNum;
@@ -12,17 +16,16 @@ public class BaseballGame {
         //Random 클래스로 난수 생성
         Random rand = new Random();
         //난수를 중복없이 저장 할 객체 생성
-        Set<Integer> hashNum = new HashSet<>();
+        Set<Integer> randNum = new HashSet<>();
         //while 문 종료를 위한 변수 선언
         boolean run = true;
 
         while (run) {
             //1~9 사이의 숫자만 생성하여 저장
-            int ranNum = rand.nextInt(9) + 1;
-            hashNum.add(ranNum);
+            randNum.add(rand.nextInt(9) + 1);
 
             //3개의 숫자가 저장될 시 종료
-            switch (hashNum.size()) {
+            switch (randNum.size()) {
                 case 3:
                     run = false;
                     break;
@@ -30,7 +33,7 @@ public class BaseballGame {
         }
 
         //랜덤하게 섞기 위하여 Hashset 을 ArrayList 로 형변환
-        correctNum = new ArrayList<>(hashNum);
+        correctNum = new ArrayList<>(randNum);
         //요소를 랜던하게 섞음
         Collections.shuffle(correctNum);
         System.out.println(correctNum);
@@ -38,75 +41,65 @@ public class BaseballGame {
 
     public int play() {
         Scanner sc = new Scanner(System.in);
-        //입력 받은 값을 저장
-        int input;
+
+        //시도하는 횟수를 저장하기 위해 변수 선언
         int tryNum = 0;
 
-        //각 자릿수를 배열로 저장하기 위해 변수 선언
-        int remainNum;
-        inputNum = new ArrayList<>();
-
-        //
         while (true) {
+
+            //입력한 값을 배열로 저장하기 위해 변수 선언
+            inputNum = new ArrayList<>();
+            Set<Integer> hashNum = new LinkedHashSet<>();
+
             //게임 진행 횟수 증가
-            tryNum ++;
+            tryNum++;
 
-            try {
-                System.out.println("숫자를 입력해주세요! : ");
-                input = sc.nextInt();
+            //입력 값을 문자형으로 저장
+            System.out.println("숫자를 입력해주세요! : ");
+            input = sc.next();
 
-                //나머지를 이용하여 각 자릿수를 배열로 저장
-                while (input > 0) {
-                    remainNum = input % 10;
-                    inputNum.add(remainNum);
-                    input = input / 10;
-                }
-                //입력된 순서를 유지하기 위해 reverse 사용
-                Collections.reverse(inputNum);
+            //문자를 숫자로 변환하여 숫자로 저장
+            for (int i = 0; i < input.length(); i++) {
+                hashNum.add((int) input.charAt(i) - '0');
+            }
+            inputNum = new ArrayList<>(hashNum);
 
-                //배열에 같은 값이 있을 경우 정제된 문자열 출력
-                if (inputNum.size() != inputNum.stream().distinct().count()) {
-                    System.out.println("올바르지 않은 입력값입니다!!");
-                    inputNum.clear();
+            //입력값이 유효하지 않을 경우 문자열 출력
+            if (!validateInput(input)) {
+                System.out.println("올바르지 않은 입력값입니다.");
+            }
 
-                    //입력받은 값이 세자리가 아닐 경우 정제된 문자 출력
-                } else if (inputNum.size() != 3 || inputNum.contains(0)) {
-                    System.out.println("올바르지 않은 입력값입니다!!");
-                    inputNum.clear();
+            //스트라이크 개수가 3개일 경우 정답 출력
+            else if (countStrike(inputNum, correctNum) == 3) {
+                System.out.println("정답입니다!!");
+                break;
+            }
 
-                }//스트라이크 개수가 3개일 경우 정답 출력
-                else if (countStrike(inputNum,correctNum) == 3){
-                    System.out.println("정답입니다!!");
-                    break;
-                }
-                //숫자와 자리 둘다 다른 경우
-                else if (countStrike(inputNum,correctNum) == 0 && countBall(inputNum,correctNum) == 0) {
-                    System.out.println("아웃");
-                    inputNum.clear();
-                }
-                //스트라이크 개수가 3개가 아닐 경우 힌트 출력
-                else {
-                    System.out.println(countStrike(inputNum,correctNum) + "스트라이크");
-                    System.out.println(countBall(inputNum,correctNum) + "볼");
-                    inputNum.clear();
-                }
+            //숫자와 자리 둘다 다른 경우
+            else if (countStrike(inputNum, correctNum) == 0 && countBall(inputNum, correctNum) == 0) {
+                System.out.println("아웃");
+            }
 
-                //숫자가 아닌 값을 입력할 경우 예외처리
-            } catch (InputMismatchException e) {
-                sc.nextLine();
-                System.out.println("올바르지 않은 입력값입니다!!");
+            //스트라이크 개수가 3개가 아닐 경우 힌트 출력
+            else {
+                System.out.println(countStrike(inputNum, correctNum) + "스트라이크");
+                System.out.println(countBall(inputNum, correctNum) + "볼");
             }
         }
         System.out.println(inputNum);
         return tryNum;
     }
 
+    //입력 받은 값 유효성 검사
     protected boolean validateInput(String input) {
+        if (Pattern.matches("^[1-9]*$", input) && inputNum.size() == 3) {
+            return true;
+        }
         return false;
     }
 
     //스트라이크 개수 계산
-    private int countStrike(List<Integer>inputNum, List<Integer>correctNum) {
+    private int countStrike(List<Integer> inputNum, List<Integer> correctNum) {
         int count = 0;
         for (int i = 0; i < inputNum.size(); i++) {
             if (inputNum.get(i) == correctNum.get(i)) {
@@ -117,9 +110,9 @@ public class BaseballGame {
     }
 
     //볼 개수 계산
-    private int countBall(List<Integer>inputNum, List<Integer>correctNum) {
+    private int countBall(List<Integer> inputNum, List<Integer> correctNum) {
         int strike;
-        strike = countStrike(inputNum,correctNum);
+        strike = countStrike(inputNum, correctNum);
         inputNum.retainAll(correctNum);
         return inputNum.size() - strike;
     }
